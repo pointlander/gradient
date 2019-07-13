8952e8d7665012a6111d1a3e09b869c0d5be15a2
Andrew Snodgrass <git@pointlander.info>
2019-07-13 11:32:35 -0600

    Added operations

--- gradient_test.go
+++ gradient_test.go
@@ -1,4 +1,4 @@
-package main
+package gradient
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
+package gradient
 
 import (
-	"fmt"
 	"math"
 )
 
@@ -48,6 +47,16 @@ func Add(a, b *V) func(k Continuation) {
 	}
 }
 
+// Sub subtracts two numbers
+func Sub(a, b *V) func(k Continuation) {
+	return func(k Continuation) {
+		c := V{a.X - b.X, 0}
+		k(&c)
+		a.D += c.D
+		b.D -= c.D
+	}
+}
+
 // Mul multiplies two numbers
 func Mul(a, b *V) func(k Continuation) {
 	return func(k Continuation) {
@@ -55,6 +64,62 @@ func Mul(a, b *V) func(k Continuation) {
 		k(&c)
 		a.D += b.X * c.D
 		b.D += a.X * c.D
+	}
+}
+
+// Div divides two numbers
+func Div(a, b *V) func(k Continuation) {
+	return func(k Continuation) {
+		c := V{a.X / b.X, 0}
+		k(&c)
+		a.D += c.D / b.X
+		b.D -= (c.D * a.X) / (b.X * b.X)
+	}
+}
+
+// Sin the sine of a number
+func Sin(a *V) func(k Continuation) {
+	return func(k Continuation) {
+		c := V{math.Sin(a.X), 0}
+		k(&c)
+		a.D += c.D * math.Cos(a.X)
+	}
+}
+
+// Cos the cosine of a number
+func Cos(a *V) func(k Continuation) {
+	return func(k Continuation) {
+		c := V{math.Cos(a.X), 0}
+		k(&c)
+		a.D -= c.D * math.Sin(a.X)
+	}
+}
+
+// Exp the base e exponential
+func Exp(a *V) func(k Continuation) {
+	return func(k Continuation) {
+		c := V{math.Exp(a.X), 0}
+		k(&c)
+		a.D += c.D * c.X
+	}
+}
+
+// Log the natural logarithm
+func Log(a *V) func(k Continuation) {
+	return func(k Continuation) {
+		c := V{math.Log(a.X), 0}
+		k(&c)
+		a.D += c.D / c.X
+	}
+}
+
+// Sigmoid the sigmoid of a number
+func Sigmoid(a *V) func(k Continuation) {
+	return func(k Continuation) {
+		i := math.Exp(a.X)
+		c := V{i / (i + 1), 0}
+		k(&c)
+		a.D += c.D * c.X * (1 - c.X)
 	}
 }
 
@@ -95,8 +160,22 @@ func U(op Unary) func(a Meta) Meta {
 var (
 	// AddOp adds two numbers
 	AddOp = B(Add)
+	// SubOp subtracts two numbers
+	SubOp = B(Sub)
 	// MulOp multiplies two numbers
 	MulOp = B(Mul)
+	// DivOp divides two numbers
+	DivOp = B(Div)
+	// SinOp the sine of a number
+	SinOp = U(Sin)
+	// CosOp the cosine of a number
+	CosOp = U(Cos)
+	// ExpOp the base e exponential
+	ExpOp = U(Exp)
+	// LogOp the natural logarithm
+	LogOp = U(Log)
+	// SigmoidOp the sigmoid of a number
+	SigmoidOp = U(Sigmoid)
 	//TanHOp the hyperbolic tangent of a number
 	TanHOp = U(TanH)
 )
@@ -106,8 +185,4 @@ func Gradient(a Meta) Continuation {
 	return a(func(a *V) {
 		a.D = 1
 	})
-}
-
-func main() {
-	fmt.Println("gradient")
 }
