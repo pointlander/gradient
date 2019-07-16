cd09a50d5f7897e06a6334f79d6732a0b4dc7a7a
Andrew Snodgrass <git@pointlander.info>
2019-07-16 16:24:45 -0600

    Improved xor network test

--- tf64/gradient_test.go
+++ tf64/gradient_test.go
@@ -5,8 +5,11 @@
 package tf64
 
 import (
+	"math"
 	"math/rand"
 	"testing"
+
+	"github.com/pointlander/gradient/sf64"
 )
 
 func TestMul(t *testing.T) {
@@ -42,6 +45,22 @@ func TestXORNetwork(t *testing.T) {
 	random64 := func(a, b float64) float64 {
 		return (b-a)*rand.Float64() + a
 	}
+
+	type Weight struct {
+		sf64.V
+		Delta float64
+	}
+
+	i1, i2, o, weights := sf64.V{}, sf64.V{}, sf64.V{}, [9]Weight{}
+	for i := range weights {
+		weights[i].X = random64(-1, 1)
+	}
+	n1 := sf64.SigmoidOp(sf64.AddOp(sf64.AddOp(sf64.MulOp(i1.Value(), weights[0].Value()), sf64.MulOp(i2.Value(), weights[1].Value())), weights[2].Value()))
+	n2 := sf64.SigmoidOp(sf64.AddOp(sf64.AddOp(sf64.MulOp(i1.Value(), weights[3].Value()), sf64.MulOp(i2.Value(), weights[4].Value())), weights[5].Value()))
+	n3 := sf64.SigmoidOp(sf64.AddOp(sf64.AddOp(sf64.MulOp(n1, weights[6].Value()), sf64.MulOp(n2, weights[7].Value())), weights[8].Value()))
+	ds := sf64.SubOp(n3, o.Value())
+	costs := sf64.MulOp(ds, ds)
+
 	input := V{
 		X: make([]float64, 2),
 		D: make([]float64, 2),
@@ -73,17 +92,33 @@ func TestXORNetwork(t *testing.T) {
 		S: []int{1, 1},
 	}
 	parameters := []*V{&w1, &b1, &w2, &b2}
+	w1.X[0] = weights[0].X
+	w1.X[1] = weights[1].X
+	b1.X[0] = weights[2].X
+	w1.X[2] = weights[3].X
+	w1.X[3] = weights[4].X
+	b1.X[1] = weights[5].X
+	w2.X[0] = weights[6].X
+	w2.X[1] = weights[7].X
+	b2.X[0] = weights[8].X
 	var deltas [][]float64
 	for _, p := range parameters {
-		for i := range p.X {
-			p.X[i] = random64(-1, 1)
-		}
 		deltas = append(deltas, make([]float64, len(p.X)))
 	}
 	l1 := SigmoidOp(AddOp(MulOp(w1.Value(), input.Value()), b1.Value()))
 	l2 := SigmoidOp(AddOp(MulOp(w2.Value(), l1), b2.Value()))
 	d := SubOp(l2, output.Value())
 	cost := MulOp(d, d)
+
+	round := func(a float64) float64 {
+		return math.Round(a*1000000) / 1000000
+	}
+	compare := func(name string, a, b float64) {
+		a, b = round(a), round(b)
+		if a != b {
+			t.Fatalf("%s %f != %f", name, a, b)
+		}
+	}
 
 	data := [...][3]float64{
 		{0, 0, 0},
@@ -97,7 +132,7 @@ func TestXORNetwork(t *testing.T) {
 			j := i + rand.Intn(len(data)-i)
 			data[i], data[j] = data[j], data[i]
 		}
-		total := 0.0
+		total, totals := 0.0, 0.0
 		for j := range data {
 			for _, p := range parameters {
 				for k := range p.D {
@@ -118,8 +153,24 @@ func TestXORNetwork(t *testing.T) {
 					p.X[l] += deltas[k][l]
 				}
 			}
+
+			i1.D, i2.D, o.D, i1.X, i2.X, o.X = 0, 0, 0, data[j][0], data[j][1], data[j][2]
+			totals += sf64.Gradient(costs).X
+			compare("w1 0", w1.D[0], weights[0].D)
+			compare("w1 1", w1.D[1], weights[1].D)
+			compare("b1 0", b1.D[0], weights[2].D)
+			compare("w1 2", w1.D[2], weights[3].D)
+			compare("w1 3", w1.D[3], weights[4].D)
+			compare("b1 1", b1.D[1], weights[5].D)
+			compare("w2 0", w2.D[0], weights[6].D)
+			compare("w2 1", w2.D[1], weights[7].D)
+			compare("b2 0", b2.D[0], weights[8].D)
+			for k := range weights {
+				weights[k].Delta, weights[k].D = alpha*weights[k].Delta-eta*weights[k].D, 0
+				weights[k].X += weights[k].Delta
+			}
 		}
-		t.Log(i, total)
+		t.Log(i, total, totals)
 		if total < .001 {
 			break
 		}
