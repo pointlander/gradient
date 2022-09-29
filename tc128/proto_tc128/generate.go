e8ebe4d7cc2c0c0753c9df44e760b9e7bd66daec
Andrew Snodgrass <git@pointlander.info>
2022-09-28 22:40:17 -0500

    Added generate

---
+++ tc128/proto_tc128/generate.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Gradient Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate protoc --go_out=. set_tc128.proto
+
+package proto_tc128
---
+++ tf64/proto_tf64/generate.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Gradient Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate protoc --go_out=. set_tf64.proto
+
+package proto_tf64
