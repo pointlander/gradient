9dd7da5093a8aff708699fb117e3e6c79e034cce
Andrew Snodgrass <pointlander@gmail.com>
2026-01-14 16:10:25 -0600

    Improvement

--- clblast/test_xor/main.go
+++ clblast/test_xor/main.go
@@ -28,19 +28,19 @@ int main() {
 	output.X[3] = 0;
 	float factor = sqrt(2.0 / ((float)w0.W));
 	for (int i = 0; i < w0.W*w0.H; i++) {
-		w0.X[i] = factor * (float)rand() / ((float)RAND_MAX+1.0);
+		w0.X[i] = factor * (2 * (float)rand() / ((float)RAND_MAX+1.0) - 1);
 	}
 	for (int i = 0; i < b0.W*b0.H; i++) {
 		b0.X[i] = 0.0;
 	}
 	factor = sqrt(2.0 / ((float)w1.W));
 	for (int i = 0; i < w1.W*w1.H; i++) {
-		w1.X[i] = factor * (float)rand() / ((float)RAND_MAX+1.0);
+		w1.X[i] = factor * (2 * (float)rand() / ((float)RAND_MAX+1.0) - 1);
 	}
 	for (int i = 0; i < b1.W*b1.H; i++) {
 		b1.X[i] = 0.0;
 	}
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 256; i++) {
 		for (int i = 0; i < w0.W*w0.H; i++) {
 			w0.D[i] = 0;
 		}
@@ -55,16 +55,16 @@ int main() {
 		}
 		gradient();
 		for (int i = 0; i < w0.W*w0.H; i++) {
-			w0.X[i] -= .1*w0.D[i];
+			w0.X[i] -= .05*w0.D[i];
 		}
 		for (int i = 0; i < b0.W*b0.H; i++) {
-			b0.X[i] -= .1*b0.D[i];
+			b0.X[i] -= .05*b0.D[i];
 		}
 		for (int i = 0; i < w1.W*w1.H; i++) {
-			w1.X[i] -= .1*w1.D[i];
+			w1.X[i] -= .05*w1.D[i];
 		}
 		for (int i = 0; i < b1.W*b1.H; i++) {
-			b1.X[i] -= .1*b1.D[i];
+			b1.X[i] -= .05*b1.D[i];
 		}
 	}
 	uninit();
