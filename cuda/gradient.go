72ed92e84835d7ce5939a8fd3e126839e86b0f71
Andrew Snodgrass <git@pointlander.info>
2026-01-30 10:23:09 -0600

    WIP cuda

---
+++ cuda/gradient.go
@@ -0,0 +1,376 @@
+// Copyright 2019 The Gradient Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cuda
+
+import (
+	"fmt"
+	"os"
+)
+
+type (
+	// V is a tensor value
+	V struct {
+		N string // the name
+		S []int  // the shape
+	}
+	// Set is a set of V
+	Set struct {
+		Weights []*V
+		ByName  map[string]*V
+	}
+	// Continuation is a continuation
+	Continuation func(a *V) bool
+	// Meta is a function that takes a continuation and return a continuation
+	Meta func(k Continuation) Continuation
+	// Unary is a unary function
+	Unary func(k Continuation, node int, a *V, options ...map[string]interface{}) bool
+	// Binary is a binary function
+	Binary func(k Continuation, node int, a, b *V, options ...map[string]interface{}) bool
+	// Operation is an operation that takes multiple parameters
+	Operation func(k Continuation, node int, a ...*V) bool
+)
+
+// Panic marks a place we should never get to
+func Panic(a *V) bool {
+	panic("should not be here")
+}
+
+// Meta returns a meta for the value
+func (a *V) Meta() Meta {
+	return func(k Continuation) Continuation {
+		k(a)
+		return Panic
+	}
+}
+
+// NewSet creates a new weight set
+func NewSet() Set {
+	return Set{
+		ByName: make(map[string]*V),
+	}
+}
+
+// Add adds weights to a set
+func (s *Set) Add(context *Context, name string, d ...int) {
+	v := NewV(context.Node, d...)
+	v.N = name
+	s.Weights = append(s.Weights, &v)
+	s.ByName[name] = &v
+}
+
+// Get gets weights from the set by name
+func (s *Set) Get(name string) Meta {
+	return s.ByName[name].Meta()
+}
+
+// Context is a function context
+type Context struct {
+	Output *os.File
+	Node   int
+}
+
+// NewV create a new tensor value
+func NewV(node int, s ...int) V {
+	if len(s) == 1 {
+		s = []int{s[0], 1}
+	}
+	return V{
+		N: fmt.Sprintf("node%d", node),
+		S: s,
+	}
+}
+
+// Allocate generates the code which allocates the variable
+func (v *V) Allocate(output *os.File) {
+	fmt.Fprintf(output, "\tcudaError_t err;\n")
+	fmt.Fprintf(output, "\tint *device_%s;\n", v.N)
+	fmt.Fprintf(output, "\tCHECK_CUDA_ERROR(cudaMalloc((void**)&device_%s, %d));\n", v.N, v.S[0]*v.S[1])
+	fmt.Fprintf(output, "\tCHECK_CUDA_ERROR(cudaMemset(device_%s, 0, %d));\n", v.N, v.S[0]*v.S[1])
+	fmt.Fprintf(output, "\tint *device_%s_d;\n", v.N)
+	fmt.Fprintf(output, "\tCHECK_CUDA_ERROR(cudaMalloc((void**)&device_%s_d, %d));\n", v.N, v.S[0]*v.S[1])
+	fmt.Fprintf(output, "\tCHECK_CUDA_ERROR(cudaMemset(device_%s_d, 0, %d));\n", v.N, v.S[0]*v.S[1])
+}
+
+// Free generates the code to free the variable
+func (v *V) Free(output *os.File) {
+	fmt.Fprintf(output, "\tCHECK_CUDA_ERROR(cudaFree(device_%s_d));\n", v.N)
+	fmt.Fprintf(output, "\tCHECK_CUDA_ERROR(cudaFree(device_%s));\n", v.N)
+}
+
+// Mul multiplies two tensors
+func (context *Context) Mul(k Continuation, node int, a, b *V, options ...map[string]interface{}) bool {
+	if len(a.S) != 2 || len(b.S) != 2 {
+		panic("tensor needs to have two dimensions")
+	}
+	width := a.S[0]
+	if width != b.S[0] {
+		panic("first dimension is not the same")
+	}
+	c := NewV(node, a.S[1], b.S[1])
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// Add adds two tensors
+func (context *Context) Add(k Continuation, node int, a, b *V, options ...map[string]interface{}) bool {
+	if len(a.S) != 2 || len(b.S) != 2 {
+		panic("tensor needs to have two dimensions")
+	}
+	width := a.S[0]
+	if width != b.S[0] {
+		panic("first dimension is not the same")
+	}
+	c := NewV(node, a.S[0], a.S[1])
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// Everett computes the split reality activation function
+func (context *Context) Everett(k Continuation, node int, a *V, options ...map[string]interface{}) bool {
+	c := NewV(node, 2*a.S[0], a.S[1])
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// Quadratic computes the quadratic cost of two tensors
+func (context *Context) Quadratic(k Continuation, node int, a, b *V, options ...map[string]interface{}) bool {
+	if len(a.S) != 2 || len(b.S) != 2 {
+		panic("tensor needs to have two dimensions")
+	}
+	width := a.S[0]
+	if width != b.S[0] || a.S[1] != b.S[1] {
+		panic("dimensions are not the same")
+	}
+	c := NewV(node, a.S[1])
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// Avg computes the average of the tensor
+func (context *Context) Avg(k Continuation, node int, a *V, options ...map[string]interface{}) bool {
+	c := NewV(node, 1)
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// T the transpose of the matrix
+func (context *Context) T(k Continuation, node int, a *V, options ...map[string]interface{}) bool {
+	c := NewV(node, a.S[1], a.S[0])
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// Dropout is a dropout regularization function
+func (context *Context) Dropout(k Continuation, node int, a *V, options ...map[string]interface{}) bool {
+	drop := .1
+	_ = drop
+	if len(options) > 0 && options[0]["drop"] != nil {
+		drop = *options[0]["drop"].(*float64)
+	}
+	c := NewV(node, a.S[0], a.S[1])
+
+	c.Allocate(context.Output)
+	defer c.Free(context.Output)
+
+	if k(&c) {
+		return true
+	}
+
+	return false
+}
+
+// Op is a operation
+func (context *Context) Op(op Operation) func(a ...Meta) Meta {
+	return func(a ...Meta) Meta {
+		node := context.Node
+		context.Node++
+		return func(k Continuation) Continuation {
+			var call func(a []Meta, b []*V) (bool, Continuation)
+			call = func(a []Meta, b []*V) (bool, Continuation) {
+				if len(a) == 0 {
+					fmt.Fprintf(context.Output, "\t{\n")
+					c := op(k, node, b...)
+					fmt.Fprintf(context.Output, "\t}\n")
+					return c, nil
+				}
+				derivatives := false
+				continuation := a[0](func(c *V) bool {
+					derivatives, _ = call(a[1:], append(b, c))
+					return derivatives
+				})
+				return derivatives, continuation
+			}
+			_, continuation := call(a, make([]*V, 0, len(a)))
+			return continuation
+		}
+	}
+}
+
+// B converts a binary function into an operator
+func (context *Context) B(op Binary) func(a, b Meta, options ...map[string]interface{}) Meta {
+	return func(a, b Meta, options ...map[string]interface{}) Meta {
+		node := context.Node
+		context.Node++
+		return func(k Continuation) Continuation {
+			return a(func(a *V) bool {
+				derivatives := false
+				b(func(b *V) bool {
+					fmt.Fprintf(context.Output, "\t{\n")
+					derivatives = op(k, node, a, b, options...)
+					fmt.Fprintf(context.Output, "\t}\n")
+					return derivatives
+				})
+				return derivatives
+			})
+		}
+	}
+}
+
+// U converts a unary function into an operator
+func (context *Context) U(op Unary) func(a Meta, options ...map[string]interface{}) Meta {
+	return func(a Meta, options ...map[string]interface{}) Meta {
+		node := context.Node
+		context.Node++
+		return func(k Continuation) Continuation {
+			return a(func(b *V) bool {
+				fmt.Fprintf(context.Output, "\t{\n")
+				derivatives := op(k, node, b, options...)
+				fmt.Fprintf(context.Output, "\t}\n")
+				return derivatives
+			})
+		}
+	}
+}
+
+// Gradient computes the gradient
+func (context *Context) Gradient(set Set, a Meta) (cost V) {
+	fmt.Fprintf(context.Output, `#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+#define CHECK_CUDA_ERROR(e) check_cuda_error(__FILE__, __LINE__, (e))
+
+void check_cuda_error(const char* file, int line, cudaError_t e) {
+	if (e == cudaSuccess) {
+		return;
+	}
+	printf("[ERROR] %%s:%%d: in function %%s\n", file, line, cudaGetErrorString(e));
+	exit(1);
+}
+
+void callback(float* output, int w, int h);
+
+struct V {
+	int W;
+	int H;
+	float *X;
+	float *D;	
+};
+`)
+	for _, value := range set.Weights {
+		fmt.Fprintf(context.Output, "__device__ struct V %s;\n", value.N)
+	}
+
+	fmt.Fprintf(context.Output, `void init(void) {
+`)
+	fmt.Fprintf(context.Output, `
+`)
+	for _, value := range set.Weights {
+		fmt.Fprintf(context.Output, "\t%s.W = %d;\n", value.N, value.S[0])
+		fmt.Fprintf(context.Output, "\t%s.H = %d;\n", value.N, value.S[1])
+		fmt.Fprintf(context.Output, "\t%s.X = (float*)calloc(%d, sizeof(float));\n", value.N, value.S[0]*value.S[1])
+		fmt.Fprintf(context.Output, "\t%s.D = (float*)calloc(%d, sizeof(float));\n", value.N, value.S[0]*value.S[1])
+	}
+	fmt.Fprintf(context.Output, `}
+void uninit(void) {
+`)
+	for _, value := range set.Weights {
+		fmt.Fprintf(context.Output, "\tfree(%s.X);\n", value.N)
+		fmt.Fprintf(context.Output, "\tfree(%s.D);\n", value.N)
+	}
+	fmt.Fprintf(context.Output, `
+}
+int gradient(void) {
+	cudaError_t err;
+`)
+	for _, value := range set.Weights {
+		fmt.Fprintf(context.Output, "\tint *device_%s\n", value.N)
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMalloc((void**)&device_%s, %d));\n", value.N, value.S[0]*value.S[1])
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMemcpy(device_%s, %s.X, %d * sizeof(float), cudaMemcpyHostToDevice));\n",
+			value.N, value.N, value.S[0]*value.S[1])
+		fmt.Fprintf(context.Output, "\tint *device_%s_d\n", value.N)
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMalloc((void**)&device_%s_d, %d));\n", value.N, value.S[0]*value.S[1])
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMemcpy(device_%s_d, %s.D, %d * sizeof(float), cudaMemcpyHostToDevice));\n",
+			value.N, value.N, value.S[0]*value.S[1])
+	}
+	a(func(a *V) bool {
+		fmt.Fprintf(context.Output, "\t{\n")
+		fmt.Fprintf(context.Output, "\tfloat* host_%s = (float*)calloc(%d, sizeof(float));\n", a.N, a.S[0]*a.S[1])
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMemcpy(device_%s, host_%s, %d * sizeof(float), cudaMemcpyDeviceToHost));\n",
+			a.N, a.N, a.S[0]*a.S[1])
+		fmt.Fprintf(context.Output, "\tcallback(host_%s, %d, %d);\n", a.N, a.S[0], a.S[1])
+		fmt.Fprintf(context.Output, `	for (int i = 0; i < %d; i++) {
+			host_%s[i] = 1.0;
+	}
+`, a.S[0]*a.S[1], a.N)
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMemcpy(device_%s_d, host_%s, %d * sizeof(float), cudaMemcpyHostToDevice));\n",
+			a.N, a.N, a.S[0]*a.S[1])
+		fmt.Fprintf(context.Output, "\tfree(host_%s);\n", a.N)
+		fmt.Fprintf(context.Output, "\t}\n")
+		return false
+	})
+	for _, value := range set.Weights {
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaFree(device_%s));\n", value.N)
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaMemcpy(device_%s_d, %s.D, %d * sizeof(float), cudaMemcpyDeviceToHost));\n",
+			value.N, value.N, value.S[0]*value.S[1])
+		fmt.Fprintf(context.Output, "\tCHECK_CUDA_ERROR(cudaFree(device_%s_d));\n", value.N)
+	}
+	fmt.Fprintf(context.Output, `
+}
+`)
+	return
+}
